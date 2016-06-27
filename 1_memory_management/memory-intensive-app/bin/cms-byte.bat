2919b1d35ceb5b6c72bd62cbc3704470435fc421
kslisenko <kslisenko@gmail.com>
2016-06-27 20:46:19 +0300

    Added GC logs collected when running memory intensive app

--- 1_memory_management/memory-intensive-app/bin/cms-byte.bat
+++
@@ -1,1 +0,0 @@
-java -jar -server -Xmx5g -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCCause -XX:+PrintTenuringDistribution -Xloggc:log\cms-gc.log ../target/memory-intensive-app-1.0-SNAPSHOT-jar-with-dependencies.jar byte 3000 1000 0:50,50:50,800:50,900:50
